da272572abe3d2b271e33928afd4081904c9e7c7
Matt Bell <matt@mbell.me>
2020-05-14 13:51:51 +0100

    Fix Linter

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Header, Body } from "../typography";
