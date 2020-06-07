58007ac2c7434541fc2573370783fbfa1c2adfdf
Matt Bell <matt@mbell.me>
2020-06-07 15:04:49 +0100

    Add places

--- src/features/World.jsx
+++ src/features/World.jsx
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import Globe from "react-globe.gl";
 import { useModeSelector } from "use-light-switch";
 import { Overlay } from "../components/layout";
 import { OverlayPage } from "../components/layout/Page";
 import { Body } from "../components/typography";
+import aFont from "../assets/fonts/A.json";
+import places, { DESTINATIONS, LONDON } from "./places";
+
+const modeVariables = {
+  light: {
+    globe: "https://unpkg.com/three-globe/example/img/earth-day.jpg",
+  },
+  dark: {
+    globe: "https://unpkg.com/three-globe/example/img/earth-night.jpg",
+  },
+  unset: {
+    globe: "https://unpkg.com/three-globe/example/img/earth-night.jpg",
+  },
+};
+
+const onLabelClick = (label) => {
+  console.log(label);
+};
 
 const World = () => {
-  const style = useModeSelector({
-    light: {
-      globe: "https://unpkg.com/three-globe/example/img/earth-day.jpg",
-    },
-    dark: {
-      globe: "https://unpkg.com/three-globe/example/img/earth-night.jpg",
-    },
-    unset: {
-      globe: "https://unpkg.com/three-globe/example/img/earth-night.jpg",
-    },
-  });
+  const location = useLocation();
+  const globeEl = useRef();
+  const modeStyle = useModeSelector(modeVariables);
+
+  useEffect(() => {
+    globeEl.current.pointOfView({ lat: 26, lng: 3, altitude: 4 }, 4000);
+  }, [location]);
+
+  const arcs = DESTINATIONS.map((place) => ({
+    startLat: LONDON.lat,
+    startLng: LONDON.lng,
+    endLat: place.lat,
+    endLng: place.lng,
+  }));
+
   return (
     <Overlay>
       <OverlayPage title="World">
         <Body>A list of places I've been.</Body>
       </OverlayPage>
       <Globe
-        bumpImageUrl="https://unpkg.com/three-globe/example/img/earth-topology.jpg"
-        globeImageUrl={style.globe}
+        bumpImageUrl="https://unpkg.com/three-globe/example/img/earth-topology.png"
+        globeImageUrl={modeStyle.globe}
         backgroundColor="rgba(0,0,0,0)"
+        ref={globeEl}
+        labelsData={places}
+        labelDotRadius={0.5}
+        labelTypeFace={aFont}
+        onLabelClick={(label) => onLabelClick(label)}
+        arcsData={arcs}
+        arcDashLength={0.01}
+        arcDashGap={0.02}
+        arcDashAnimateTime={4000}
       ></Globe>
     </Overlay>
   );
---
+++ src/features/places.js
@@ -0,0 +1,69 @@
+export const LONDON = {
+  lat: 51.5285582,
+  lng: -0.2416807,
+  text: "London, UK",
+};
+export const DESTINATIONS = [
+  {
+    lat: 48.8588377,
+    lng: 2.2770204,
+    text: "Paris, FR",
+  },
+  {
+    lat: 41.3947688,
+    lng: 2.0787279,
+    text: "Barcelona, ES",
+  },
+  {
+    lat: 34.7726773,
+    lng: 32.3979911,
+    text: "Paphos, CY",
+  },
+  {
+    lat: 39.1693819,
+    lng: 23.4156833,
+    text: "Skiathos, GR",
+  },
+  {
+    lat: 46.5284586,
+    lng: 6.5824551,
+    text: "Lausanne, CH",
+  },
+  {
+    lat: 50.1211277,
+    lng: 8.4964808,
+    text: "Frankfurt, DE",
+  },
+  {
+    lat: 51.4484647,
+    lng: 5.3814708,
+    text: "Eindhoven, NL",
+  },
+  {
+    lat: 52.3545828,
+    lng: 4.7638778,
+    text: "Amsterdam, NL",
+  },
+  {
+    lat: 60.1098678,
+    lng: 24.7385074,
+    text: "Helsinki, FI",
+  },
+  {
+    lat: 45.4042008,
+    lng: 12.1071497,
+    text: "Venice, IT",
+  },
+  {
+    lat: 43.7181557,
+    lng: -79.518142,
+    text: "Toronto, CA",
+  },
+  {
+    lat: 40.0292888,
+    lng: -105.3100175,
+    text: "Boulder CO, US",
+  },
+];
+
+export default [LONDON, ...DESTINATIONS];
