8ce84617ba902167c2d1f5a92ae728a7fa051395
Matt Bell <matt@mbell.dev>
2020-07-26 17:52:56 +0100

    Add fetching recent blog posts

--- src/features/Blog.jsx
+++ src/features/Blog.jsx
@@ -1,18 +1,44 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 import { Page } from "../components/layout";
+import { SubHeader, Body, Link } from "../components/typography"
 
 /* eslint-disable import/no-webpack-loader-syntax */
 import Document from "!babel-loader!mdx-loader!../pages/blog.md";
 
-class Home extends Component {
-  render() {
+const apiURL = "https://mbell-api.glitch.me/posts/";
+
+const Post = ({ title, excerpt, url }) => <section style={{display: "flex", flexDirection: "column"}}>
+  <SubHeader>{title}</SubHeader>
+  <Body>{excerpt}</Body>
+  <Body><Link to={url}>Read more</Link></Body>
+</section>
+
+const Home = () => {
+  const location = useLocation();
+  const [ posts, setPosts ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const res = await fetch(apiURL);
+      const json = await res.json();
+      setPosts(json);
+      setIsLoading(false);
+    })();
+  }, [location])
+
     return (
       <Page title="Blog">
         <Document />
+      {isLoading && (
+        <Body>Loading latest posts...</Body>
+        )}
+        {posts.map(post => <Post {...post}/>)}
       </Page>
     );
-  }
 }
 
 export default Home;
